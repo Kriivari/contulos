e46765062e7e18fc94cba8a3f6e915856517c4ea
Kalle Kivimaa <killer@basen.net>
2018-07-03 19:06:44 +0300

    Fixes from Hannis

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,7 +2,7 @@ class Tournament < ApplicationRecord
   has_many :results
 
   def genres
-    %w[Bofferiturnaus Figupelit Korttipelit KPS-turnaus Lautapelit Piirrustuskilpailu Pukukilpailu Realms\ of\ Ropecon Skenariokilpailu Taidenäyttely]
+    %w[Bofferiturnaus Figupelit Korttipelit KPS-turnaus Lautapelit Pukukilpailu Skenaariokilpailu Taidenäyttely]
   end
 
 end
