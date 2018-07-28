0f27fc81197803908ee0d09b4e3ca773c51e0f4f
Kalle Kivimaa <killer@basen.net>
2018-07-29 00:28:15 +0300

    Sort entries

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,5 @@
 class Tournament < ApplicationRecord
-  has_many :results, order: 'place'
+  has_many :results, -> { order('place') }
 
   def genres
     %w[Figupelit Korttipelit Lautapelit Skenaariokilpailu Bofferiturnaus KPS-turnaus Pukukilpailu Taidenäyttely]
