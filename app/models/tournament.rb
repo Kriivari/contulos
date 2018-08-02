5e6b83f21bfdca35194928f2004cbd592237dc98
Kalle Kivimaa <killer@basen.net>
2018-08-02 09:16:10 +0300

    Add others category

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,8 +1,9 @@
+# coding: utf-8
 class Tournament < ApplicationRecord
   has_many :results, -> { order('place') }
 
   def genres
-    %w[Figupelit Korttipelit Lautapelit Skenaariokilpailu Bofferiturnaus KPS-turnaus Pukukilpailu Taidenäyttely]
+    %w[Figupelit Korttipelit Lautapelit Skenaariokilpailu Bofferiturnaus KPS-turnaus Pukukilpailu Taidenäyttely Muut]
   end
 
 end
